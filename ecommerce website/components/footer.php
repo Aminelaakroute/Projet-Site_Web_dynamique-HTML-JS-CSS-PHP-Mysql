ac3e8d72bc2f6aec24230222ea323d1ce8c5a141
Aminelaakroute <116645925+Aminelaakroute@users.noreply.github.com>
2023-03-11 01:49:50 +0100

    Add files via upload

---
+++ ecommerce website/components/footer.php
@@ -0,0 +1,41 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>Liens rapides</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> contact</a>
+      </div>
+
+      <div class="box">
+         <h3>Liens supplémentaires</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> cart</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> orders</a>
+      </div>
+
+      <div class="box">
+         <h3>Nous contacter</h3>
+         <a href="tel:1234567890"><i class="fas fa-phone"></i> +123 456 7899</a>
+         <a href="tel:11122233333"><i class="fas fa-phone"></i> +111 222 3333</a>
+         <a href="mailto:4AR.Ecom@gmail.com"><i class="fas fa-envelope"></i> 4AR.Ecom@gmail.com</a>
+         <a href="https://www.google.com/myplace"><i class="fas fa-map-marker-alt"></i> Tanger, Maroc - 149756 </a>
+      </div>
+
+      <div class="box">
+         <h3>Suivez nous</h3>
+         <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+         <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+         <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>4.A.R</span> | all rights reserved!</div>
+
+</footer>
